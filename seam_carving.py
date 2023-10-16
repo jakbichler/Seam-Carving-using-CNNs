eb4854497193b4484b640bf3b5c25f18e5fd3d60
jakob <j.d.bichler@student.tudelft.nl>
2023-10-16 11:28:15 +0200

    Initial implementation of seam carving, Fixes in modify_features to clip values

--- seam_carving.py
+++ seam_carving.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils import data
 from torchvision.models import vgg19
 from torchvision import transforms
@@ -11,7 +12,7 @@ import cv2
 import argparse
 from PIL import Image
 from models.vgg19.vgg import CustomVGG
-from utils import overlay_heatmap, modify_features
+from utils import overlay_heatmap, modify_features, get_seam, highlight_seam_heatmap, highlight_seam_image
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--image_path', type=str, help='Path to the input image')
@@ -29,31 +30,58 @@ if __name__ == '__main__':
     model.eval()
 
     # Load an image
-    img = Image.open(image_path)
+    orig_image = Image.open(image_path)
+    orig_img_cv2 = cv2.imread(image_path)
+
+    ic(orig_image.size)
 
     # save the shape of the image
-    orig_img_shape = img.size
-
+    orig_img_shape = orig_image.size
 
     # Transform and preprocess the image
-    img = torch.FloatTensor(model.transform(img).unsqueeze(0))
+    img = torch.FloatTensor(model.transform(orig_image).unsqueeze(0))
 
     # Generate the Grad-CAM heatmap
     heatmap = model.grad_cam_heatmap(class_id, img)
 
-    # # Display the heatmap using matplotlib
-    # plt.matshow(heatmap.squeeze())
-    # plt.show()
+    # Print number of heatmap entries that are zero
+    ic((heatmap == 0).sum())
 
+    # Resize the heatmap to twice the size for better painitng experience
+    # heatmap = F.interpolate(heatmap.unsqueeze(0).unsqueeze(0), size=(2 * heatmap.shape[0], 2 * heatmap.shape[1]), mode='bilinear', align_corners=False)
 
-    # # Overlay the heatmap on the original image
-    # overlayed_image = overlay_heatmap(heatmap, image_path)
-
-    # # Convert the overlayed image to the appropriate depth
-    # overlayed_image = cv2.convertScaleAbs(overlayed_image)
-    
-    # # Display the overlayed image using matplotlib
-    # plt.imshow(cv2.cvtColor(overlayed_image, cv2.COLOR_BGR2RGB))
-    # plt.show()
-
+    # Modify the feature map by painting on it (value range is 0-1)
     inpainted_heatmap = modify_features(img, heatmap.squeeze(), image_path, orig_img_shape)
+
+    # Resize the heatmap to the original image size
+    inpainted_heatmap = F.interpolate(inpainted_heatmap.unsqueeze(0).unsqueeze(0), size=(orig_img_shape[1], orig_img_shape[0]), mode='bilinear', align_corners=False)
+
+    
+    plt.matshow(inpainted_heatmap.squeeze())
+    plt.title("inpainted_heatmap")
+    plt.show()
+
+
+
+
+    ## Implementation of seam_carving
+    # # Convert the heatmap to a numpy array
+    heatmap_np = inpainted_heatmap.squeeze().detach().cpu().numpy()
+
+    # Calculate the first seam
+    M, backtrack = get_seam(heatmap_np)
+    heatmap_np = np.uint8(255 * heatmap_np)
+
+
+    # display the heatmap with the highlighted seam
+    heatmap_highlighted_seam = highlight_seam_heatmap(heatmap_np, M, backtrack)
+    plt.matshow(heatmap_highlighted_seam)
+    plt.title("highlighted_heatmap")
+    plt.show()
+
+
+    # display the original image with the highlighted seam
+    image_highlighted_seam = highlight_seam_image(orig_img_cv2, M, backtrack)
+    plt.imshow(cv2.cvtColor(cv2.convertScaleAbs(image_highlighted_seam),  cv2.COLOR_BGR2RGB))
+    plt.title("highlighted_image")
+    plt.show()
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt 
+import torch
+import torch.nn.functional as F
 from icecream import ic
 
 
@@ -18,34 +20,6 @@ def overlay_heatmap(heatmap, image_path):
 
 
 
-# def modify_features(image, heatmap, image_path):
-    
-#     plt_image = image[0].permute(1, 2, 0).detach().cpu().numpy() / 2 + 0.5
-#     coords = []
-#     heatmap_new = heatmap.clone()
-
-#     def onclick(event):
-#         ix, iy = event.xdata, event.ydata
-#         print(f'Painted at: (x: {ix}, y: {iy})')
-
-#         # Modify the heatmap
-#         heatmap_new[int(iy), int(ix)] += 0.1
-
-
-#         # Update the displayed image with the modified heatmap
-#         imshow.set_data(heatmap_new.squeeze().detach().cpu().numpy())
-#         fig.canvas.draw()
-
-#     fig = plt.figure()
-#     ax = fig.add_subplot(111)
-#     cid = fig.canvas.mpl_connect('button_press_event', onclick)
-    
-#     imshow = ax.imshow(heatmap_new.squeeze().detach().cpu().numpy())
-#     plt.show()
-
-#     return heatmap_new
-
-
 
 def modify_features(image, heatmap, image_path, orig_img_shape):
     
@@ -53,26 +27,33 @@ def modify_features(image, heatmap, image_path, orig_img_shape):
     coords = []
     heatmap_new = heatmap.clone()
 
+    # heatmap_new = F.interpolate(heatmap_new.unsqueeze(0).unsqueeze(0), size=orig_img_shape, mode='bilinear', align_corners=False)
+
+
     def onclick(event):
         ix, iy = event.xdata, event.ydata
-
 
         # Modify the heatmap
         ic(plt_image.shape)
         # rescale ix and iy to fit heatmap size
-        ix = ix / orig_img_shape[0] * heatmap_new.shape[1]
-        iy = iy / orig_img_shape[1] * heatmap_new.shape[0]
+        ix_heat = ix / orig_img_shape[0] * heatmap_new.shape[0]
+        iy_heat = iy / orig_img_shape[1] * heatmap_new.shape[1]
 
-        print(f'Painted at: (x: {ix}, y: {iy})')
+        print(f'Painted at: (x: {ix:.1f}, y: {iy:.1f}, corresponding to heatmap coordinates: (x: {int(ix_heat)}, y: {int(iy_heat)})')
 
-        heatmap_new[int(iy), int(ix)] += 0.1
+        # Clip heatmap to max 1
+        if heatmap_new[int(iy_heat), int(ix_heat)] < 0.8:
+            heatmap_new[int(iy_heat), int(ix_heat)] += 0.2
+
+        else:
+            heatmap_new[int(iy_heat), int(ix_heat)] = 1
 
 
         # Update the displayed image with the modified heatmap
         imshow.set_data(cv2.cvtColor(cv2.convertScaleAbs(overlay_heatmap(heatmap_new, image_path)),  cv2.COLOR_BGR2RGB))
         fig.canvas.draw()
 
-    fig = plt.figure()
+    fig = plt.figure("Impaint featuremap")
     ax = fig.add_subplot(111)
     cid = fig.canvas.mpl_connect('button_press_event', onclick)
     
@@ -84,3 +65,107 @@ def modify_features(image, heatmap, image_path, orig_img_shape):
     plt.show()
 
     return heatmap_new
+
+
+
+# Inspired by https://karthikkaranth.me/blog/implementing-seam-carving-with-python/
+def get_seam(heatmap):
+    r, c = heatmap.shape
+
+    M = heatmap.copy()
+    backtrack = np.zeros_like(M, dtype=int)
+
+    for i in range(1, r):
+        for j in range(0, c):
+            # Handle the left edge of the image, to ensure we don't index -1
+            if j == 0:
+                idx = np.argmin(M[i - 1, j:j + 2])
+                backtrack[i, j] = idx + j
+                min_energy = M[i - 1, idx + j]
+            else:
+                idx = np.argmin(M[i - 1, j - 1:j + 2])
+                backtrack[i, j] = idx + j - 1
+                min_energy = M[i - 1, idx + j - 1]
+
+            M[i, j] += min_energy
+
+    return M, backtrack
+
+
+
+# Inspired by https://karthikkaranth.me/blog/implementing-seam-carving-with-python/
+def carve_column(img):
+    r, c, _ = img.shape
+
+    M, backtrack = get_seam(img)
+
+    # Create a (r, c) matrix filled with the value True
+    # We'll be removing all pixels from the image which
+    # have False later
+    mask = np.ones((r, c), dtype=np.bool)
+
+    # Find the position of the smallest element in the
+    # last row of M
+    j = np.argmin(M[-1])
+
+    for i in reversed(range(r)):
+        # Mark the pixels for deletion
+        mask[i, j] = False
+        j = backtrack[i, j]
+
+    # Since the image has 3 channels, we convert our
+    # mask to 3D
+    mask = np.stack([mask] * 3, axis=2)
+
+    # Delete all the pixels marked False in the mask,
+    # and resize it to the new image dimensions
+    img = img[mask].reshape((r, c - 1, 3))
+
+    return img
+
+
+
+
+def highlight_seam_heatmap(img, M, backtrack):
+    r, c = img.shape
+    mask = np.zeros((r, c), dtype=bool)
+ 
+
+    highlighted_heatmap = img.copy()
+
+
+    # Find the starting point with the lowest energy in the last row
+    j = np.argmin(M[-1])
+    for i in range(r - 1, -1, -1):
+        mask[i, j] = True
+        j = backtrack[i, j]
+
+    # Highlight the seam 
+    highlighted_heatmap[mask == True] = 255  
+
+    return highlighted_heatmap
+
+
+
+def highlight_seam_image(img, M, backtrack):
+
+    ic(img.shape)
+    r, c, _ = img.shape
+    mask = np.zeros((r, c), dtype=bool)
+
+    highlighted_image = img.copy()
+
+
+    # Find the starting point with the lowest energy in the last row
+    j = np.argmin(M[-1])
+    for i in range(r - 1, -1, -1):
+        mask[i, j] = True
+        j = backtrack[i, j]
+
+    # Highlight the seam in red, the image ix width, height, 3 channels. red to 255
+
+        highlighted_image[mask == True, 0] = 0
+        highlighted_image[mask == True, 1] = 165
+        highlighted_image[mask == True, 2] = 200
+
+    return highlighted_image
